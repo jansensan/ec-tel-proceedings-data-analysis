9a036325688e323ba7a8fc0853ed601fd5083c73
Mat Janson Blanchet <mat.jb@jansensan.net>
2019-01-04 13:04:17 -0500

    Added <ContinentDistributionChart> (#18)
    Signed-off-by: Mat Janson Blanchet <mat.jb@jansensan.net>

---
+++ src/components/continent-distribution-chart/continent-distribution-chart.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import {Bar} from 'react-chartjs-2';
+
+// constants
+import CSVFileNames from '../../constants/csv-file-names';
+
+// models
+import authorsModel from '../../models/authors-model';
+import dataModel from '../../models/data-model';
+import continentDistModel from './continent-distribution-model';
+
+// components
+import DownloadChartButton from '../download-chart-button/download-chart-button.jsx';
+import DownloadCSVButton from '../download-csv-button/download-csv-button.jsx';
+
+// styles
+require('./continent-distribution-chart.scss');
+
+
+export default class ContinentDistributionChart extends Component {
+  constructor(props) {
+    super(props);
+    this.chartId = 'continentDistChart';
+    this.state = {
+      isComponentMounted: false,
+    };
+
+    dataModel.updated.add(this.onDataUpdated, this);
+  }
+
+  // react methods definitions
+  render() {
+    return (
+      <div className="continent-distribution-chart">
+        {
+          (this.state.hasData > 0) &&
+          <div>
+            <Bar
+              id={this.chartId}
+              data={continentDistModel.data}
+              options={continentDistModel.options}
+              redraw={true}
+            />
+            <DownloadChartButton
+              targetId={this.chartId}
+            />
+            <DownloadCSVButton
+              buttonLabel="Download CSV"
+              fileName={CSVFileNames.CONTINENT_DISTRIBUTION}
+              getCSV={continentDistModel.getCSV.bind(continentDistModel)}
+            />
+          </div>
+        }
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.setState({
+      isComponentMounted: true
+    });
+  }
+
+
+  // methods definitions
+  onDataUpdated() {
+    if (!dataModel.hasData()) {
+      return;
+    }
+    continentDistModel.updateData({
+      2016: authorsModel.getNumAuthorsPerCountry(2016),
+      2017: authorsModel.getNumAuthorsPerCountry(2017),
+      2018: authorsModel.getNumAuthorsPerCountry(2018)
+    });
+
+    this.setState({
+      hasData: true
+    });
+  }
+}
---
+++ src/components/continent-distribution-chart/continent-distribution-model.js
@@ -0,0 +1,180 @@
+import _ from 'lodash';
+
+// constants
+import Continents from '../../constants/continents';
+
+// services
+import DatasetFactory from '../../services/dataset-factory';
+import CountryService from '../../services/country-service';
+
+// models
+import authorsModel from '../../models/authors-model';
+
+
+class ContinentDistributionModel {
+  constructor() {
+    this.data = {
+      labels: [],
+      datasets: [
+        DatasetFactory.createOrangeDataset('2018'),
+        DatasetFactory.createBlueDataset('2017'),
+        DatasetFactory.createGreenDataset('2016'),
+      ]
+    };
+    this.options = {
+      tooltips: {
+        callbacks: {
+          label: this.getTooltipLabel.bind(this)
+        }
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'authors (%)'
+          }
+        }]
+      },
+      maintainAspectRatio: false
+    }
+  }
+
+  createContinentValueObject() {
+    let vo = {};
+    _.forEach(
+      Continents,
+      (continent) => {
+        vo[continent.id] = {
+          id: continent.id,
+          name: CountryService.getContinentNameWithId(continent.id),
+          numAuthors: 0,
+        };
+      }
+    );
+    return vo;
+  }
+
+  getContinentsLabels() {
+    let labels = [];
+    _.forEach(
+      Continents,
+      (continent) => {
+        labels.push(continent.name);
+      }
+    );
+    return labels;
+  }
+
+  getCSV() {
+    console.info('--- getCSV ---');
+
+    let sep = '\t';
+    let rows = ['continent', '2018', '2017', '2016'];
+    let csvContent = 'data:text/csv;charset=utf-8,'
+      + rows.join(sep) + '\n';
+
+    let numContinents = 6;
+    for (let i = 0; i < numContinents; i++) {
+      let row = this.data.labels[i]
+        // 2018
+        + sep + this.data.datasets[0].data[i]
+        // 2017
+        + sep + this.data.datasets[1].data[i]
+        // 2016
+        + sep + this.data.datasets[2].data[i];
+      csvContent += row + '\n';
+    }
+
+    console.log('csvContent: ', csvContent);
+
+    return csvContent;
+  }
+
+  getTooltipLabel(tooltipItem) {
+    let dataset = this.data.datasets[tooltipItem.datasetIndex];
+    let val = dataset.data[tooltipItem.index];
+    let label = _.round(val, 1) + '%'
+      + ' (' + dataset.label + ')';
+    return label;
+  }
+
+  updateData(newData) {
+    // create continents data
+    let continents = {
+      2016: this.createContinentValueObject(),
+      2017: this.createContinentValueObject(),
+      2018: this.createContinentValueObject(),
+    };
+
+    // parse data
+    _.forEach(
+      newData[2016],
+      (country) => {
+        let continentId = CountryService.getContinentIdWithCountryCode(country.name);
+        continents[2016][continentId].numAuthors += country.numAuthors;
+      }
+    );
+    _.forEach(
+      newData[2017],
+      (country) => {
+        let continentId = CountryService.getContinentIdWithCountryCode(country.name);
+        continents[2017][continentId].numAuthors += country.numAuthors;
+      }
+    );
+    _.forEach(
+      newData[2018],
+      (country) => {
+        let continentId = CountryService.getContinentIdWithCountryCode(country.name);
+        continents[2018][continentId].numAuthors += country.numAuthors;
+      }
+    );
+
+    let numAuthors = 0;
+
+    // assign distribution 2016
+    numAuthors = authorsModel.getNumAuthors(2016);
+    let values2016 = [];
+    _.forEach(
+      continents[2016],
+      (continent) => {
+        values2016.push((continent.numAuthors / numAuthors) * 100);
+      }
+    );
+
+    // assign distribution 2017
+    numAuthors = authorsModel.getNumAuthors(2017);
+    let values2017 = [];
+    _.forEach(
+      continents[2017],
+      (continent) => {
+        values2017.push((continent.numAuthors / numAuthors) * 100);
+      }
+    );
+
+    // assign distribution 2018
+    numAuthors = authorsModel.getNumAuthors(2018);
+    let values2018 = [];
+    _.forEach(
+      continents[2018],
+      (continent) => {
+        values2018.push((continent.numAuthors / numAuthors) * 100);
+      }
+    );
+
+    // set labels
+    this.data.labels = this.getContinentsLabels();
+
+    // set data
+    this.data.datasets[0].data = values2018;
+    this.data.datasets[1].data = values2017;
+    this.data.datasets[2].data = values2016;
+  }
+}
+
+
+// create and export singleton
+let continentDistModel = new ContinentDistributionModel();
+export default continentDistModel;
